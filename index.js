679bc732cbf4387713c6d6e45230774630b03c82
Andrew Masih <armasih@outlook.com>
2021-05-09 22:05:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*  
+ variable a and 
+ variable A are two
+ different variables because
+ Javascript differentiate 
+ lowercased and upercased 
+ names
+*/
+
+var a = 5;
+var A = 7;
+
+/*
+Variable totalSize is camelCased. 
+
+*/
+var totalSize = 100;
+
+console.log("a is set to: " + a);
+console.log("But A is set to: " + A);
+console.log(totalSize);
